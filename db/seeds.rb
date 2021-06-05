d0e844dff9c9feb85b53cc2fa2e59f329ac80072
hahunavth <vuthanhha.2001@gmail.com>
2021-06-06 04:25:57 +0700

    fix seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,6 +15,6 @@ NguoiDan.create(id: 3,ten: 'Nguyen Van C', so_cmnd: '030201001122', so_ho_khau: 
 User.create(id: 1,name: 'admin', password: '1', password_confirmation: '1', permision: 0 )
 User.create(id: 2,name: '030201001123', password: '1', password_confirmation: '1', permision: 1 )
 
-TamTru.create(  id: 1, nguoi_dan_id: 3, tu_ngay: Sat, 05 Jun 2021, den_ngay: Sat, 05 Jun 2021, dia_chi: "Ha noi", li_do: "@@@", xac_nhan: nil )
+# TamTru.create(  id: 1, nguoi_dan_id: 3, tu_ngay: Sat, 05 Jun 2021, den_ngay: Sat, 05 Jun 2021, dia_chi: "Ha noi", li_do: "@@@", xac_nhan: nil )
 
-TamVang.create(  id: 1, nguoi_dan_id: 3, tu_ngay: Sat, 05 Jun 2021, den_ngay: Sat, 05 Jun 2021, dia_chi: "Hai Duong", li_do: "@@@", xac_nhan: nil )
+# TamVang.create(  id: 1, nguoi_dan_id: 3, tu_ngay: Sat, 05 Jun 2021, den_ngay: Sat, 05 Jun 2021, dia_chi: "Hai Duong", li_do: "@@@", xac_nhan: nil )
